c9d37313c826f672e70b141584a01934379eaead
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-11-04 19:24:16 +0600

    Add files via upload

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -9,21 +9,10 @@ public class TrianglePattern {
         int rows;
         System.out.println("Enter a Rows number for the pattern");
         rows = scan.nextInt();
-        System.out.println(""); //print a new line 
+        System.out.println("\n");
         
-        //for loop to build the pattern
-        
-        for (int i = rows; i >= 1; --i) {  
-            //for loop that creates rows
+        for (int i = rows; i >= 1; --i) {
             for (int j = 1; j <= i; ++j) {
-                
-                /*for  loop to make the pattern like
-                1 2 3 4
-                1 2 3
-                1 2
-                1 
-                for row number 4*/
-                
                 System.out.print(j+" ");
             }
         System.out.println();
