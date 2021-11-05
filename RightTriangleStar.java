06c8e455e052f196dd3d30629170eb0bf886b64c
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-11-05 22:06:21 +0600

    Add files via upload

---
+++ RightTriangleStar.java
@@ -0,0 +1,20 @@
+package righttrianglestar;
+import java.util.Scanner;
+public class RightTriangleStar {
+    public static void main(String[] args) {
+        int rows;
+        
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter the number of rows : ");
+        rows = scan.nextInt();
+        
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        System.out.println();
+
+        }
+    
+    }
+}
